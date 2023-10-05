24f216cc32cecb3397f806f7f05f2c90e53b3b85
Hassan Aijaz <hassanaijaz233@gmail.com>
2023-10-05 23:49:58 +0500

    Add Get, Set, Insert, Remove and reverse method in linked list.

---
+++ LinkedListDS/LinkedList.cs
@@ -0,0 +1,215 @@
+﻿namespace LinkedListDS
+{
+    public class LinkedList
+    {
+        Node? head;
+        Node? tail;
+        private int length;
+        public LinkedList()
+        {
+            head = null;
+            tail = null;
+            length = 0;
+        }
+        public Node? GetHead()
+        {
+            return head;
+        }
+        public Node? GetTail()
+        {
+            return tail;
+        }
+        public int Length()
+        {
+            return length;
+        }
+        public void PrintLinkedList()
+        {
+            Node? temp = head;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.value);
+                temp = temp.next;
+            }
+        }
+        public void Append(int value)
+        {
+            //Check if we got any item in our linked list. if not we create a new node and point head and tail to new node.
+            Node node = new(value);
+            if (length == 0) // linked list is empty
+            {
+                head = node;
+                tail = node;
+                length++;
+                return;
+            }
+
+            //If link list is not empty then append a item at the end and change the pointer of the tail to new node.
+            if (tail != null)
+            {
+                tail.next = node;
+                tail = node;
+                length++;
+                return;
+            }
+        }
+        public void Prepend(int value)
+        {
+            Node node = new(value);
+            //Check if we got any item in our linked list. if not we create a new node and point head and head to new node.
+            if (length == 0) // linked list is empty
+            {
+                head = node;
+                tail = node;
+                length++;
+                return;
+            }
+            node.next = head;
+            head = node;
+            length++;
+        }
+        public Node? RemoveLast()
+        {
+            if (length == 0) // linked list is empty
+            {
+                return null;
+            }
+
+            if (head == tail)
+            {
+                head = null;
+                tail = null;
+                length--;
+                return null;
+            }
+            if (head is not null && tail is not null)
+            {
+                Node temp = head;
+                Node pre = head;
+                while (temp.next != null)
+                {
+                    pre = temp;
+                    temp = temp.next;
+                }
+                tail = pre;
+                tail.next = null;
+                length--;
+                return temp;
+            }
+            return null;
+        }
+        public Node? RemoveFirst()
+        {
+            if (length == 0)
+            {
+                return null;
+
+            }
+
+            if (head == tail)
+            {
+                length--;
+                return null;
+            }
+
+            if (head is not null && tail is not null)
+            {
+                Node temp = head;
+                head = head.next;
+                return temp;
+            }
+            return null;
+        }
+        public Node? Get(int index)
+        {
+            if (index < 0 || index >= length)
+            {
+                return null;
+            }
+            var temp = head;
+            for (int i = 0; i < index; i++)
+            {
+                temp = temp.next;
+            }
+            return temp;
+        }
+        public Node? Set(int index, int value)
+        {
+            if (index < 0 || index >= length) { return null; }
+            var node = Get(index);
+            if (node == null) return null;
+            node.value = value;
+            return node;
+        }
+        public bool Insert(int index, int value)
+        {
+            if (index < 0 || index > length)
+            {
+                return false;
+            }
+            if (index == 0)
+            {
+                Prepend(value);
+                return true;
+            }
+            if (index == length)
+            {
+                Append(value);
+                return true;
+            }
+            Node node = new Node(value);
+            var temp = Get(index - 1);
+            if (temp == null) return false;
+            node.next = temp.next;
+            temp.next = node;
+            length++;
+            return true;
+        }
+        public Node? Remove(int index)
+        {
+            if (index < 0 || index >= length) { return null; }
+            if (index == 0)
+            {
+                return RemoveFirst();
+            }
+            if (index == length - 1)
+            {
+                return RemoveLast();
+            }
+            var prevNode = Get(index - 1);
+            var nodeToRemove = prevNode != null ? prevNode.next : null;
+            if (prevNode == null || nodeToRemove == null) return null;
+            //0 1 2 3 4 5
+            prevNode.next = nodeToRemove.next;
+            nodeToRemove.next = null;
+            length--;
+            return nodeToRemove;
+        }
+        public void Reverse()
+        {
+            Node? temp = head;
+            head = tail;
+            tail = temp;
+            Node? after = temp?.next;
+            Node? before = null;
+            for (int i = 0; i < length; i++)
+            {
+                after = temp.next;
+                temp.next = before;
+                before = temp;
+                temp = after;
+            }
+
+
+        }
+    }
+    public class Node
+    {
+        public int value;
+        public Node? next;
+        public Node(int value)
+        {
+            this.value = value;
+        }
+    }
+}
--- LinkedListDS/Program.cs
+++ LinkedListDS/Program.cs
@@ -20,17 +20,24 @@ namespace LinkedListDS
                 Console.WriteLine("tail: " + tail.value);
             }
             Console.WriteLine("len: " + linkedList.Length());
-            linkedList.Append(1);
-            linkedList.Append(2);
-            linkedList.Append(3);
-            linkedList.Append(4);
-            linkedList.Append(5);
-            linkedList.Prepend(0);
-            linkedList.Prepend(-1);
-            linkedList.Prepend(-2);
+            linkedList.Prepend(012);
+            linkedList.Prepend(-9);
+            linkedList.Prepend(-1232);
+            linkedList.Append(11);
+            linkedList.Append(22);
+            linkedList.Append(33);
+            linkedList.Append(44);
+            linkedList.Append(55);
+            Console.WriteLine("Get:" + linkedList.Get(5)?.value);
+            Console.WriteLine("Set:" + linkedList.Set(5, -1231231)?.value);
+            Console.WriteLine("Get:" + linkedList.Get(5)?.value);
+            linkedList.Insert(5, 120);
+            Console.WriteLine("Remove:" + linkedList.Remove(5)?.value);
 
-            int? value = linkedList.RemoveFirst()?.value;
-            Console.WriteLine("Remove first node: "+value);
+
+            //int? value = linkedList.RemoveFirst()?.value;
+            //Console.WriteLine("Remove first node: "+value);
+            Console.WriteLine("Before the reverse");
             linkedList.PrintLinkedList();
 
             head = linkedList.GetHead();
@@ -44,147 +51,21 @@ namespace LinkedListDS
             {
                 Console.WriteLine("tail: " + tail.value);
             }
+            Console.WriteLine("After the reverse");
+            linkedList.Reverse();
+            linkedList.PrintLinkedList();
 
-        }
-    }
-    public class Node
-    {
-        public int value;
-        public Node? next;
-        public Node(int value)
-        {
-            this.value = value;
-        }
-    }
-    public class LinkedList
-    {
-        Node? head;
-        Node? tail;
-        private int length;
-
-
-        public LinkedList()
-        {
-            head = null;
-            tail = null;
-            length = 0;
-        }
-
-        public Node? GetHead()
-        {
-            return head;
-        }
-
-        public Node? GetTail()
-        {
-            return tail;
-        }
-
-        public int Length()
-        {
-            return length;
-        }
-
-        public void PrintLinkedList()
-        {
-            Node? temp = head;
-            while (temp != null)
+            head = linkedList.GetHead();
+            tail = linkedList.GetTail();
+            Console.WriteLine("len: " + linkedList.Length());
+            if (head != null)
             {
-                Console.WriteLine(temp.value);
-                temp = temp.next;
+                Console.WriteLine("head: " + head.value);
             }
-        }
-
-        public void Append(int value)
-        {
-            //Check if we got any item in our linked list. if not we create a new node and point head and tail to new node.
-            Node node = new(value);
-            if (length == 0) // linked list is empty
-            {
-                head = node;
-                tail = node;
-                length++;
-                return;
-            }
-
-            //If link list is not empty then append a item at the end and change the pointer of the tail to new node.
             if (tail != null)
             {
-                tail.next = node;
-                tail = node;
-                length++;
-                return;
+                Console.WriteLine("tail: " + tail.value);
             }
-        }
-
-        public void Prepend(int value)
-        {
-            Node node = new(value);
-            //Check if we got any item in our linked list. if not we create a new node and point head and head to new node.
-            if (length == 0) // linked list is empty
-            {
-                head = node;
-                tail = node;
-                length++;
-                return;
-            }
-            node.next = head;
-            head = node;
-            length++;
-        }
-
-        public Node? RemoveLast()
-        {
-            if (length == 0) // linked list is empty
-            {
-                return null;
-            }
-
-            if (head == tail)
-            {
-                head = null;
-                tail = null;
-                length--;
-                return null;
-            }
-            if (head is not null && tail is not null)
-            {
-                Node temp = head;
-                Node pre = head;
-                while (temp.next != null)
-                {
-                    pre = temp;
-                    temp = temp.next;
-                }
-                tail = pre;
-                tail.next = null;
-                length--;
-                return temp;
-            }
-            return null;
-        }
-
-        public Node? RemoveFirst()
-        {
-            if (length == 0)
-            {
-                return null;
-
-            }
-
-            if (head == tail)
-            {
-                length--;
-                return null;
-            }
-
-            if(head is not null && tail is not null)
-            {
-                Node temp = head;
-                head = head.next;
-                return temp;
-            }
-            return null;
         }
     }
 }
